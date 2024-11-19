948f22c0a53683812b3a2fc4f9aee056987928a0
WUKE7 <kevin.wu@oocl.com>
2024-11-19 15:29:47 +0800

    test, should initialize mars rover

---
+++ src/test/java/MarsRoverTest.java
@@ -0,0 +1,13 @@
+import org.example.MarsRover;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarsRoverTest {
+    @Test
+    void should_initialize_mars_rover() {
+        // Given
+        MarsRover rover = new MarsRover();
+    }
+
+}
