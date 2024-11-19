290a423323945e31704858c310a7ee148f5d0f20
WUKE7 <kevin.wu@oocl.com>
2024-11-19 16:21:15 +0800

    refactor, Modified the member variables of MarsRover

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -3,11 +3,13 @@ package org.example;
 public class MarsRover {
     private static int coordinateX = 0;
     private static int coordinateY = 0;
-    private static String direction = "N";
+    private static int directionIndex = 0;
+    private static final String[] Directions= {"N","E","S","W"};
 
     public String showStatus() {
-        return coordinateX + ":" + coordinateY + ":" + direction;
+            return coordinateX + ":" + coordinateY + ":" + Directions[directionIndex];
     }
+
 
     public String ChangeMarsRoverOriented(String command) {
         return null;
