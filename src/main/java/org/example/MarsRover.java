8a08260fd9ba10a13b0de35fba66815e3c7a2717
WUKE7 <kevin.wu@oocl.com>
2024-11-19 15:56:06 +0800

    feat, create showStatus function return the origin position of mars rover

--- src/main/java/org/example/MarsRover.java
+++ src/main/java/org/example/MarsRover.java
@@ -1,5 +1,11 @@
 package org.example;
 
 public class MarsRover {
+    private static int coordinateX = 0;
+    private static int coordinateY = 0;
+    private static String direction = "N";
 
+    public String showStatus() {
+        return coordinateX + ":" + coordinateY + ":" + direction;
+    }
 }
--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -8,7 +8,10 @@ class MarsRoverTest {
     void should_initialize_mars_rover() {
         // Given
         MarsRover rover = new MarsRover();
-
+        // When
+        String report = rover.showStatus();
+        //Then
+        assertEquals("0:0:N",report);
     }
 
 }
